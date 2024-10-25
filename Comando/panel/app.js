613dccb4f8716755b49164cd267a1e2dc203994f
EduardoMVirgilio <eduardomiguelvirgilio@gmail.com>
2024-10-25 18:02:11 -0300

    Update panel Comando

--- Comando/panel/app.js
+++ Comando/panel/app.js
@@ -1,5 +1,11 @@
 window.addEventListener('message', function (event) {
     if (event.data.type === "showCommands") {
         document.getElementById("comandos").innerText = event.data.commands;
+
+        // Cerrar el panel automáticamente después de 5 segundos
+        setTimeout(function () {
+            // Enviar un mensaje para cerrar el panel
+            window.dispatchEvent(new MessageEvent('message', { data: { type: 'close' } }));
+        }, 5000); // 5000 milisegundos = 5 segundos
     }
 });
