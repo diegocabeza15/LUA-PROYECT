903e8ff303faa16e0f7a96c9e1386deac70df052
EduardoMVirgilio <eduardomiguelvirgilio@gmail.com>
2024-10-25 20:51:07 -0300

    Init Gamemode

--- gamemode/basic_client.lua
+++ gamemode/basic_client.lua
@@ -1,4 +1,26 @@
+local model = GetHashKey('a_f_m_fatcult_01') 
+
+spawnpoint 'a_m_y_hipster_01' { -1772.3885, 444.3736, 127.2703, 119.0825 }
+
 AddEventHandler('onClientMapStart', function()
   exports.spawnmanager:setAutoSpawn(true)
   exports.spawnmanager:forceRespawn()
+end)
+
+-- Permitir fuego amigo
+Citizen.CreateThread(function()
+  while true do
+      Wait(0)
+      NetworkSetFriendlyFireOption(true)
+      SetCanAttackFriendly(PlayerPedId(), true, false)
+  end
+end)
+
+-- Manejador del evento de spawn del jugador
+AddEventHandler('playerSpawned', function()
+  local playerPed = PlayerPedId()
+  
+  -- Configurar spawn en una ubicación específica
+  local spawnCoords = vector3(-1767.9266, 445.3730, 127.2791, 106.2196) -- Cambia estas coordenadas según lo necesites
+  SetEntityCoords(playerPed, spawnCoords.x, spawnCoords.y, spawnCoords.z, false, false, false, true)
 end)
--- gamemode/fxmanifest.lua
+++ gamemode/fxmanifest.lua
@@ -2,11 +2,10 @@
 -- Altering or recreating for local use only is strongly discouraged.
 
 version '1.0.0'
-author 'Cfx.re <root@cfx.re>'
+author 'author'
 description 'A basic freeroam gametype that uses the default spawn logic from spawnmanager.'
-repository 'https://github.com/citizenfx/cfx-server-data'
 
-resource_type 'gametype' { name = 'Freeroam' }
+resource_type 'gametype' { name = 'Mondongo Land' }
 
 client_script 'basic_client.lua'
 
